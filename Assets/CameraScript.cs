2895d6d44b8d46c3a479bacb943517f942c77bdf
testdebugged <sie85050@gmail.com>
2025-03-20 20:00:16 +1030

    upd mar 20

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -5,7 +5,7 @@ public class CameraScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float mouseSensitivity = 100f;
     public Transform playerBody;
-    
+    public bool cursorLockState = true; 
     float xRotation = 0f;
     
     void Start()
@@ -23,6 +23,7 @@ public class CameraScript : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         
+        Cursor.lockState = cursorLockState ? CursorLockMode.None : CursorLockMode.Locked;
         playerBody.Rotate(Vector3.up * mouseX); 
     }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,16 +7,20 @@ public class Player : MonoBehaviour
     public float speed = 1f;
     public float gravity = -1.81f;
     public float jump = 1f;
+    public float forceMultiplier = 10f; // for dragging objects
     public Transform groundTransform;
     public float groundDistance = 0.4f;
 
     public LayerMask GroundLayer;
     public LayerMask RayLayer;
     public Camera PlayerCamera;
+    public GameObject Axis;
 
     Vector3 velocity; // fall velocity
     bool isGrounded;
-    private GameObject selectedObject; // for dragging obj?
+    private Rigidbody selectedObject; // for dragging obj?
+
+    public GameObject prefabObject;
    
     enum moveState
     {
@@ -65,24 +69,82 @@ public class Player : MonoBehaviour
             // velocity.z = move.z * speed * Mathf.Abs(gravity);
         }
         velocity.y += gravity * Time.deltaTime;
-        if (True)//Movement != moveState.Jump)
+        if (true)//Movement != moveState.Jump)
         {
             characterController.Move(move * (Movement == moveState.Run ? speed*4 : speed)); // ternary
         } 
         characterController.Move(velocity);
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Mouse0))
         {
-            emitRay();
+            instantiateObject();
+            // #nullable disable
+            // RaycastHit spawnLocation = emitRay();
+            // if (spawnLocation != null) {
+            //     instantiateObject(prefabObject, spawnLocation.point);
+            // }
+            // #nullable enable
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            dragObject();
+        }
+
+        if (selectedObject != null)
+        {
+            float forceStrength = Vector3.Distance(Axis.transform.position, selectedObject.transform.position); //Mathf.Atan2(Axis.transform.position.x - selectedObject.transform.position.x, Axis.transform.position.y - selectedObject.transform.position.y);
+            Vector3 resultantForce = ((Axis.transform.position - selectedObject.transform.position).normalized * forceStrength) * forceMultiplier;
+            selectedObject.AddForce(resultantForce);
         }
     }
 
-    void emitRay()
+    void instantiateObject(float _range = Mathf.Infinity)
     {
         RaycastHit hit;
-        if (Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.forward, out hit, Mathf.Infinity))
+        if(Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.forward, out hit, _range))
         {
-            Debug.Log(hit.point);
+            Instantiate(prefabObject, hit.point, PlayerCamera.transform.rotation);
         }
     }
+
+    void dragObject(float _range = Mathf.Infinity)
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.forward, out hit, _range))
+        {
+            if (hit.rigidbody)
+            {
+                selectedObject = hit.rigidbody;
+                Debug.Log(selectedObject.GetComponent<Rigidbody>());
+                return;
+            }
+        }
+        selectedObject = null;
+    }
+
+    // RaycastHit emitRay(float _range = Mathf.Infinity)
+    // {
+    //     RaycastHit hit;
+    //     if(!Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.forward, out hit, _range)) {
+    //         return null;
+    //     }
+    //     return hit;
+    //         //Instantiate(prefabObject, hit.point, PlayerCamera.transform.rotation);
+    // }
+
+    // void instantiateObject(GameObject _object, Vector3 _origin, Quaternion _rotation)
+    // {
+    //     if (_origin != null && _object != null)
+    //     {
+    //         Instantiate(_object, _origin, _rotation);
+    //     }
+    // }
+
+    // void instantiateObject(GameObject _object, Vector3 _origin)
+    // {
+    //     if (_origin != null && _object != null)
+    //     {
+    //         Instantiate(_object, _origin, PlayerCamera.transform.rotation);
+    //     }
+    // }
 }
