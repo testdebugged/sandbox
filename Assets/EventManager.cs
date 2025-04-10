d7f19917125e2a2ac90833f170adba79e5458b5f
testdebugged <sie85050@gmail.com>
2025-04-10 19:26:12 +0930

    april 10

--- Assets/EventManager.cs
+++ Assets/EventManager.cs
@@ -10,7 +10,7 @@ public class EventManager : MonoBehaviour
     // EventManager for managing events. object list can be swapped out with array/prefab or resource list, maybe?
     // will act as the middleman
     public DragEntities DragScript;
-    //public SpawnEntities SpawnScript; // obj to spawn should be decided from player monobehaviour
+    public SpawnEntities SpawnScript; // obj to spawn should be decided from player monobehaviour
     //public DeleteEntities DeleteScript;
     public Camera playerCamera;
     public void execute(int value = 0)
--- Assets/Player/Tools/SpawnEntities.cs
+++ Assets/Player/Tools/SpawnEntities.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class SpawnEntities : MonoBehaviour
 {
+        public Camera PlayerCamera;
+        public GameObject prefabObject;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +15,18 @@ public class SpawnEntities : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void mouseClicked()
+    {
+        instantiateObject();
+    }
+    void instantiateObject(float _range = Mathf.Infinity)
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.forward, out hit, _range))
+        {
+            Instantiate(prefabObject, hit.point, PlayerCamera.transform.rotation);
+        }
     }
 }
